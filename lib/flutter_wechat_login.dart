3665e65da87e29554d66a3025e07a47c8d6d6802
Sun Jiaxuan <sunjiaxuan@hotmail.com>
2024-06-05 23:34:05 +0800

    [ :sparkles: ] : 添加 函数 使用Flutter重写

--- lib/flutter_wechat_login.dart
+++ lib/flutter_wechat_login.dart
@@ -1,11 +1,25 @@
-
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 import 'flutter_wechat_login_platform_interface.dart';
 
 class FlutterWechatLogin {
-  Future<void> init({required String appId, required String secret, String? universalLink}) {
-    return FlutterWechatLoginPlatform.instance.init(appId: appId, secret: secret, universalLink: universalLink);
+  static final FlutterWechatLogin _instance = FlutterWechatLogin._internal();
+
+  factory FlutterWechatLogin() {
+    return _instance;
+  }
+
+  FlutterWechatLogin._internal();
+
+  late String _appId;
+  late String _secret;
+
+  init({required String appId, required String secret, String? universalLink}) {
+    _appId = appId;
+    _secret = secret;
+    return FlutterWechatLoginPlatform.instance
+        .init(appId: appId, secret: secret, universalLink: universalLink);
   }
 
   Future<bool> isInstalled() {
@@ -20,36 +34,49 @@ class FlutterWechatLogin {
     return {};
   }
 
-  Future<Map<String, dynamic>> getAccessToken({required String code}) async {
-    String data = await FlutterWechatLoginPlatform.instance.getAccessToken(code: code) ?? "";
-    if (data.isNotEmpty) {
-      return jsonDecode(data);
+  Future<Map<String, dynamic>> getAccessToken(String code) async {
+    final url = Uri.parse(
+        'https://api.weixin.qq.com/sns/oauth2/access_token?appid=$_appId&secret=$_secret&code=$code&grant_type=authorization_code');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to get access token');
     }
-    return {};
   }
 
-  Future<Map<String, dynamic>> refreshToken({required String refreshToken}) async {
-    String data = await FlutterWechatLoginPlatform.instance.refreshToken(refreshToken: refreshToken) ?? "";
-    if (data.isNotEmpty) {
-      return jsonDecode(data);
+  Future<Map<String, dynamic>> checkToken(
+      String accessToken, String openid) async {
+    final url = Uri.parse(
+        'https://api.weixin.qq.com/sns/auth?access_token=$accessToken&openid=$openid');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to check token');
     }
-    return {};
   }
 
-  Future<Map<String, dynamic>> checkToken({required String accessToken, required String openid}) async {
-    String data = await FlutterWechatLoginPlatform.instance.checkToken(accessToken: accessToken, openid: openid) ?? "";
-    if (data.isNotEmpty) {
-      return jsonDecode(data);
+  Future<Map<String, dynamic>> refreshToken(String refreshToken) async {
+    final url = Uri.parse(
+        'https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=$_appId&grant_type=refresh_token&refresh_token=$refreshToken');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to refresh token');
     }
-    return {};
   }
 
-  Future<Map<String, dynamic>> getUserInfo({required String accessToken, required String openid}) async {
-    String data = await FlutterWechatLoginPlatform.instance.getUserInfo(accessToken: accessToken, openid: openid) ?? "";
-    if (data.isNotEmpty) {
-      return jsonDecode(data);
+  Future<Map<String, dynamic>> getUserInfo(
+      String accessToken, String openid) async {
+    final url = Uri.parse(
+        'https://api.weixin.qq.com/sns/userinfo?access_token=$accessToken&openid=$openid');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to get user info');
     }
-    return {};
   }
-
 }
